371849e8f824d1d6941599b8ffdccc13750d8d7a
Ryan <drdgvhbh@gmail.com>
2019-03-25 00:07:48 -0400

    holy shit it works?

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,34 +1,51 @@
 import { boundMethod } from 'autobind-decorator';
-import automerge, { DocSet, getActorId } from 'automerge';
+import automerge from 'automerge';
 import Immutable, { Map, Set } from 'immutable';
 import Peer from 'peerjs';
-import { any } from 'prop-types';
 import React, { Component } from 'react';
-import { Block, Document, Operation, Value } from 'slate';
+import { Operation, Value } from 'slate';
 import {
-  SerializedBlock,
-  SerializedDocument,
-  SerializedInline,
-  serializeDocument,
-  SerializedText,
+  applyAutomergeOperations,
+  applySlateOperations,
+  automergeJsonToSlate,
+  slateCustomToJson,
 } from 'slate-automerge';
 import { Editor } from 'slate-react';
 import './App.css';
-import initialValueJSON from './initialValue.json';
-import logo from './logo.svg';
+import { initialValue as initialValueJSON } from './initialValue';
 
 const initialValue = Value.fromJSON(initialValueJSON as any);
+
+let dumb = automerge.init();
+const initialSlateValue = initialValue;
+dumb = automerge.change(dumb, 'Initialize Slate state', (doc: any) => {
+    doc.note = slateCustomToJson(initialSlateValue.document);
+});
 
 interface InitialConnectionMessage {
   type: 'INITIAL_CONNECTION_MESSAGE';
   peerID: string;
 }
 
+interface InitialStateMessage {
+  type: 'INITIAL_STATE_MESSAGE';
+  initialState: string;
+}
+
+interface ChangeMessage {
+  type: 'CHANGE';
+  changeData: string;
+}
+
 interface AppState {
   value: Value;
-  peers: Set<string>;
+  peers: Map<string, Peer.DataConnection>;
   connectingPeerID: string;
   peerID: string;
+}
+
+interface Doc {
+  value: any;
 }
 
 class App extends Component<{}, AppState> {
@@ -36,73 +53,97 @@ class App extends Component<{}, AppState> {
 
   private self!: Peer;
 
-  private docSet!: DocSet;
-
-  private connection!: automerge.Connection;
+  private docSet!: automerge.DocSet;
 
   constructor(props: any) {
     super(props);
+
+    this.docSet = new automerge.DocSet();
 
     this.state = {
       value: initialValue,
       peerID: '',
       connectingPeerID: '',
-      peers: Set([]),
+      peers: Map(),
     };
   }
 
   public componentDidMount(): void {
-    this.docSet = new automerge.DocSet();
 
     this.self = new Peer({
       secure: true,
-      host: 'a26d3ebb.ngrok.io',
+      host: 'df806975.ngrok.io',
       port: 443,
       path: '/swag',
     });
-    const initialDoc = automerge.init();
     this.self.on('open', (peerID) => {
       console.log(peerID);
-      this.docSet.setDoc(peerID, initialDoc);
-      this.connection = new automerge.Connection(this.docSet, (msg: any) => {
-        console.log('automerge connection receiving a msg', msg);
-        const { peers } = this.state;
-
-        peers.forEach((peer) => {
-          const intermediaryConnection = this.self.connect(peer!);
-          intermediaryConnection.on('open', () => {
-            intermediaryConnection.send(msg);
-          });
-        });
-
-        this.connection.open();
-      });
       this.setState({ peerID });
-      const doc = this.docSet.getDoc(peerID);
-      const newDoc = automerge.change(
-        doc,
-        'Initialize',
-        (oldDoc: { [key: string]: any }) => {
-          oldDoc.value = serializeDocument(initialValue.document);
+
+      const dsadad = automerge.init();
+      const initialDoc = automerge.change(
+        dsadad,
+        'Initialize Slate state',
+        (doc: { value: any; }) => {
+          const { value } = this.state;
+          console.log('slate to json', slateCustomToJson(value.document));
+          doc.value = slateCustomToJson(value.document);
         },
       );
-      this.docSet.setDoc(peerID, newDoc);
+      this.docSet.setDoc(peerID, initialDoc);
     });
     this.self.on('connection', (conn) => {
-      conn.on('data', (data: InitialConnectionMessage) => {
-        switch (data.type) {
-          case 'INITIAL_CONNECTION_MESSAGE':
-            const { peers } = this.state;
-            if (!peers.contains(data.peerID)) {
-              this.connectToPeer(data.peerID);
+      conn.on(
+        'data',
+        (
+          data: InitialConnectionMessage | ChangeMessage | InitialStateMessage,
+        ) => {
+          const { peerID } = this.state;
+          switch (data.type) {
+            case 'INITIAL_CONNECTION_MESSAGE': {
+              const { peers } = this.state;
+              if (!peers.has(data.peerID)) {
+                this.connectToPeer(data.peerID);
+              }
+              break;
             }
-            break;
-          default:
-            break;
-        }
-        // Will print 'hi!'
-        console.log('Incoming Transmission!', data);
-      });
+            case 'INITIAL_STATE_MESSAGE': {
+              const doc = JSON.parse(data.initialState);
+              const newDoc = automerge.applyChanges(automerge.init(), doc);
+
+              this.docSet.setDoc(peerID, newDoc);
+
+              break;
+            }
+            case 'CHANGE': {
+              const currentDoc = this.docSet.getDoc(peerID);
+
+              const changes = JSON.parse(data.changeData);
+
+              const newDoc = automerge.applyChanges(currentDoc!!, changes);
+              const opSetDiff = automerge.diff(currentDoc!!, newDoc);
+
+              if (opSetDiff.length !== 0) {
+                let change = (this.state.value as any).change();
+                change = applyAutomergeOperations(opSetDiff, change, () => {
+                  const doc = this.docSet.getDoc(this.state.peerID);
+                  const newJson = automergeJsonToSlate({
+                    document: { ...doc!!.value },
+                  })!!;
+                  this.setState({ value: Value.fromJSON(newJson) });
+                });
+                if (change) {
+                  this.setState({ value: change.value });
+                }
+              }
+              this.docSet.setDoc(peerID, newDoc);
+              break;
+            }
+            default:
+              break;
+          }
+        },
+      );
     });
   }
 
@@ -118,7 +159,12 @@ class App extends Component<{}, AppState> {
         <button onClick={() => this.connectToPeer(connectingPeerID)}>
           Call
         </button>
-        <Editor value={this.state.value} onChange={this.onChange} />
+        <Editor
+          value={this.state.value}
+          onChange={({ value, operations }) => {
+            return this.onChange({ value, operations });
+          }}
+        />
       </div>
     );
   }
@@ -130,46 +176,38 @@ class App extends Component<{}, AppState> {
   }: {
     operations: Immutable.List<Operation>;
     value: Value;
-  }) {
+  }): void {
     this.setState({ value });
 
     const { peerID } = this.state;
     const currentDoc = this.docSet.getDoc(peerID);
 
-    const newDoc = automerge.change(currentDoc, 'apply changes', (doc: any) => {
-      operations.forEach((operation) => {
-        if (!operation) {
-          return;
-        }
+    if (!currentDoc) {
+      return;
+    }
 
-        const rootNode: SerializedDocument = doc.value;
+    console.log(currentDoc);
+    applySlateOperations(this.docSet, peerID, operations, '');
+    const newDoc = this.docSet.getDoc(peerID)!!;
 
-        switch (operation.type) {
-          case 'insert_text':
-            const { path, offset, text } = operation;
-            let currentNode:
-              | SerializedDocument
-              | SerializedBlock
-              | SerializedInline
-              | SerializedText = rootNode;
-            path.forEach((el) => {
-              if (!this.isLeafNode(currentNode)) {
-                currentNode = currentNode.nodes[el];
-              }
-            });
-            if (this.isLeafNode(currentNode)) {
-              const firstLeaf = currentNode.leaves[0];
-              firstLeaf.text.insertAt(offset, text);
-            }
-            break;
-          default:
-            break;
-        }
+    const changeData = JSON.stringify(automerge.getChanges(currentDoc, newDoc));
+
+    if (isExtendedDoc(newDoc)) {
+      const { peers } = this.state;
+
+      peers.keySeq().forEach((peer) => {
+        const intermediaryConnection = this.self.connect(peer!);
+        intermediaryConnection.on('open', () => {
+          const changeMessage: ChangeMessage = {
+            type: 'CHANGE',
+            changeData,
+          };
+          intermediaryConnection.send(changeMessage);
+        });
       });
-    });
-    this.docSet.setDoc(peerID, newDoc);
-
-    this.connection.sendMsg(peerID, Map<any, any>(), newDoc);
+    } else {
+      throw new Error(`doc is not extended doc, ${newDoc}`);
+    }
   }
 
   @boundMethod
@@ -177,27 +215,35 @@ class App extends Component<{}, AppState> {
     const connection = this.self.connect(connectingPeerID);
     connection.on('open', () => {
       const { peers, peerID } = this.state;
-      this.setState({ peers: peers.add(connectingPeerID) });
+
+      this.setState({ peers: peers.set(connectingPeerID, connection) });
       const initialConnectionMsg: InitialConnectionMessage = {
         type: 'INITIAL_CONNECTION_MESSAGE',
         peerID,
       };
       connection.send(initialConnectionMsg);
+      this.sendInitialState(connectingPeerID);
     });
   }
 
-  private isLeafNode(
-    node:
-      | SerializedDocument
-      | SerializedBlock
-      | SerializedInline
-      | SerializedText,
-  ): node is SerializedText {
-    return (
-      (node as SerializedDocument | SerializedBlock | SerializedInline)
-        .nodes === undefined
+  private sendInitialState(recipientPeerID: string): void {
+    const { peers, peerID } = this.state;
+    const currentDoc = this.docSet.getDoc(peerID);
+    const connection = peers.get(recipientPeerID);
+    const changeData = JSON.stringify(
+      automerge.getChanges(automerge.init(), currentDoc!!),
     );
+    const initialStateMessage: InitialStateMessage = {
+      type: 'INITIAL_STATE_MESSAGE',
+      initialState: changeData,
+    };
+
+    connection!!.send(initialStateMessage);
   }
+}
+
+function isExtendedDoc(doc: any): doc is Doc {
+  return doc.value !== undefined;
 }
 
 export default App;
